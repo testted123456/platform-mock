5fadb8c743b54492aa40a25b476edb4bc1ff097e
黄伟 <H.W.@HW-2.local>
2018-04-16 10:25:13 +0800

    修改代码以适配springboot项目以外部tomcat启动

--- src/main/java/com/nonobank/test/WebMockApplication.java
+++ src/main/java/com/nonobank/test/WebMockApplication.java
@@ -2,12 +2,23 @@ package com.nonobank.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class WebMockApplication {
+public class WebMockApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebMockApplication.class, args);
     }
-}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // TODO Auto-generated method stub
+        builder.sources(this.getClass());
+        return super.configure(builder);
+
+    }
+
+    }
